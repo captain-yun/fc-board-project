10900b2d603b273787acb4335ff1af787e723d5c
captain-yun <jslovejs182@gmail.com>
2023-11-23 13:21:59 +0900

    Auto stash before merge of "main" and "origin/main"
    #8 - 도메인 클래스 설계 (게시글, 댓글)
    도메인 정보를 설계해봄. 기초적인 게시글과 댓글 표현이 가능하도록. DB와 연동을 위한  추가적인 표현은 추후에 업데이트될 예정.

---
+++ board-project/src/main/java/com/kitri/boardproject/domain/ArticleComment.java
@@ -0,0 +1,14 @@
+package com.kitri.boardproject.domain;
+
+import java.time.LocalDateTime;
+
+public class ArticleComment {
+    private Long id;
+    private Article article;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime modifiedAt;
+    private String modifiedBy;
+}
