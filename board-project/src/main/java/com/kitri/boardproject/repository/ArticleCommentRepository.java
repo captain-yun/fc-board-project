0f9fe0bdaafee85963631a1a2c1e107600f6b1e1
captain-yun <jslovejs182@gmail.com>
2023-11-24 20:27:42 +0900

    #10 - 게시글, 댓글 레포지토리 data rest 로 탐지
    Data rest가 레포지토리 탐지하여 api로 만들 수 있도록 어노테이션 붙임.
    @RepositoryRestResource

--- board-project/src/main/java/com/kitri/boardproject/repository/ArticleCommentRepository.java
+++ board-project/src/main/java/com/kitri/boardproject/repository/ArticleCommentRepository.java
@@ -2,6 +2,9 @@ package com.kitri.boardproject.repository;
 
 import com.kitri.boardproject.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
